65fbb0cdfae317319c9588811d7998bbc595b823
konrad <konradkar2@gmail.com>
2022-05-22 15:13:48 +0200

    Add kubeadm

--- linux/kubernetes.sh
+++ linux/kubernetes.sh
@@ -4,14 +4,14 @@ echo "kubernetes.sh"
 
 cd $HOME
 
-##install docker engine https://docs.docker.com/engine/install/ubuntu/
+echo -e "\nInstalling docker engine..." #https://docs.docker.com/engine/install/ubuntu/
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -19,7 +19,18 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
+echo -e "\nTesting docker engine..."
 sudo docker run hello-world
 
+echo -e "\nInstalling kubeadm, kubectl, kubelet" #https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo -e "\nFetching repositories..."
 git clone https://github.com/Orange-OpenSource/towards5gs-helm.git
 git clone https://github.com/k8snetworkplumbingwg/multus-cni
