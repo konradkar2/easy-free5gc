1bace5a3522ec30058751dbf79849368813f238b
konrad <konradkar2@gmail.com>
2022-05-27 11:29:26 +0200

    add controller/init.sh config

--- controler/init.sh
+++ controler/init.sh
@@ -2,3 +2,8 @@
 
 echo "controller/init.sh"
 
+dns_ip=$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --upload-certs --control-plane-endpoint=$dns_ip
+
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
