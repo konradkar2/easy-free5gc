5dbfa63314787516e93a20df1592f3c7b6ee4a5f
konrad <konradkar2@gmail.com>
2022-05-29 11:40:19 +0200

    fix

--- controler/free5gc-setup.sh
+++ controler/free5gc-setup.sh
@@ -9,3 +9,7 @@ yq e -i '.global.n6network.subnetIP|="10.0.0.0"' values.yaml
 yq e -i '.global.n6network.cidr|=8' values.yaml
 yq e -i '.global.n6network.gatewayIP|="10.0.0.12"' values.yaml
 yq e -i '.global.n6network.excludeIP|="10.0.0.12"' values.yaml
+
+cd ..
+
+helm -n free5gc install 3.0.6 ./free5gc/
