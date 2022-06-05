dc3baa99a7b87402ae7b1d652165139345a2467b
konrad <konradkar2@gmail.com>
2022-06-05 12:01:09 +0200

    extract route configuration

--- controler/init.sh
+++ controler/init.sh
@@ -4,12 +4,6 @@ echo "controller/init.sh"
 
 ./kubernetes.sh
 
-echo "Installing helm..."
-cd $HOME/Download
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
 echo "cloning towards5gs-helm..."
 cd $HOME
 git clone https://github.com/Orange-OpenSource/towards5gs-helm.git
---
+++ worker/routing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#setup routing for eth1, this is required on GCP
+IP_ADDRESS=192.168.11.100
+NETMASK=255.255.255.255
+CIDR=32
+GATEWAY_ADDRESS=192.168.11.1
+
+sudo ifconfig eth1 $IP_ADDRESS netmask $NETMASK broadcast $IP_ADDRESS mtu 1430
+sudo echo "1 rt1" | sudo tee -a /etc/iproute2/rt_tables
+sudo ip route add $GATEWAY_ADDRESS src $IP_ADDRESS dev eth1 table rt1
+sudo ip route add default via $GATEWAY_ADDRESS dev eth1 table rt1
+sudo ip rule add from $IP_ADDRESS/$CIDR table rt1
+sudo ip rule add to $IP_ADDRESS/$CIDR table rt1
