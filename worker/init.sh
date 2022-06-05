491b90856c5fd4fc8cde74f163cf41de1ea4afe5
konrad <konradkar2@gmail.com>
2022-06-05 12:54:44 +0200

    fix invalid worker address

--- worker/init.sh
+++ worker/init.sh
@@ -7,3 +7,9 @@ echo "worker/init.sh"
 ./routing.sh
 
 ./free5gc-dependencies.sh
+
+#append node internal ip to kubelet args so worker gets proper address in k8s
+#added because address of eth1 was getting assigned
+sudo sed -i '${s/$/ --node-ip 192.168.10.3/}' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
