1c3a96b8ea1722ef48dfb364b90f6341d18a5dce
konrad <konradkar2@gmail.com>
2022-06-16 19:11:14 +0200

    fix

--- worker/init.sh
+++ worker/init.sh
@@ -11,7 +11,7 @@ echo "worker/init.sh"
 #append node internal ip to kubelet args so worker gets proper address in k8s
 #added because address of eth1 was getting assigned
 
-eth0_ip=$(ip --brief address show | grep eth0 | awk '{ print $3}' | cut -d/ >
+eth0_ip=$(ip --brief address show | grep eth0 | awk '{ print $3}' | cut -d/ -f1)
 echo $eth0_ip
 
 sudo sed -i '${s/$/ --node-ip '"$eth0_ip"'/}' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
