0f7f8543093ef0b9cd9bcee0b73e20ad4e4e8019
konrad <konradkar2@gmail.com>
2022-06-16 18:52:15 +0200

    automate node ip assignment

--- worker/init.sh
+++ worker/init.sh
@@ -10,6 +10,11 @@ echo "worker/init.sh"
 
 #append node internal ip to kubelet args so worker gets proper address in k8s
 #added because address of eth1 was getting assigned
-sudo sed -i '${s/$/ --node-ip 192.168.10.3/}' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+eth0_ip=$(ip --brief address show | grep eth0 | awk '{ print $3}' | cut -d/ >
+echo $eth0_ip
+
+sudo sed -i '${s/$/ --node-ip '"$eth0_ip"'/}' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
