158357a4ebd20f838abc9cec513eaecd937ff044
konrad <konradkar2@gmail.com>
2022-06-16 19:15:56 +0200

    fix if

--- worker/init.sh
+++ worker/init.sh
@@ -2,12 +2,13 @@
 
 echo "worker/init.sh"
 
- if [ "$#" -ne 2 ]
+if [ "$#" -ne 2 ]
     then
         echo "Please provide original names of eth0, eth1 interfaces, as they are about to be renamed."
         exit
+fi
 
-./netplan.sh $1 $2
+./netplan.sh "$1" "$2"
 
 ./routing.sh
 
@@ -17,7 +18,7 @@ echo "worker/init.sh"
 #added because address of eth1 was getting assigned
 
 eth0_ip=$(ip --brief address show | grep eth0 | awk '{ print $3}' | cut -d/ -f1)
-echo $eth0_ip
+echo "$eth0_ip"
 
 sudo sed -i '${s/$/ --node-ip '"$eth0_ip"'/}' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
